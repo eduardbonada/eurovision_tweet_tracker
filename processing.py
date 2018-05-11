250b132a8041a3c7dd0fb79326a4b2f7b5e63c59
eduardbonada <eduard.bonada@gmail.com>
2018-05-11 12:09:53 +0300

    change first tweet to compute from

--- processing.py
+++ processing.py
@@ -29,7 +29,7 @@ else:
     features_file = 'features.bin'
 
 # filter out all tweets previous to this one (like a date filter but with tweets, because the createdAt is a string difficult to filter from SQLite)
-min_tweet_id = 994124952352718849
+min_tweet_id = 993655288795459584
 # 993655288795459584 # first tweet of tuesday at 00:00
 # 994124952352718849 # first tweet of wednesday at 08:00
 # 994366557294866432 # first tweet of thursday at 00:00
